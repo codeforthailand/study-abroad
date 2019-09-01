5c67d34f2f0d5bf9fa60663225516e542f2aeafd
heytitle <pat.chormai@gmail.com>
2019-09-01 09:30:01 +0200

    fix star button

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,6 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import GitHubButton from 'react-github-btn'
-
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -15,6 +13,7 @@ const Header = ({ siteTitle }) => (
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        paddingBottom: 0,
       }}
     >
       <div style={{ margin: 0 }}>
@@ -28,12 +27,12 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
         {`  `}
-        <GitHubButton
-          href="https://github.com/codeforthailand/study-abroad"
-          data-icon="octicon-star"
-          aria-label="Star codeforthailand/study-abroad on GitHub">
-            Star
-        </GitHubButton>
+        <a href="https://github.com/codeforthailand/study-abroad">
+          <img 
+            style={{verticalAlign: "middle", height: "20px", marginTop: "20px"}}
+            src="https://img.shields.io/github/stars/codeforthailand/study-abroad?style=social"
+          />
+        </a>
       </div>
     </h3>
   </header>
