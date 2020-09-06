02007f0b69e067805ed3f4e31b7822e7515fc981
Pat Chormai <pat.chormai@gmail.com>
2020-09-06 11:41:12 +0200

    update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,4 +2,4 @@
 
 SRC="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6R4cAFsbSoOGGXV58jabkAnuN70b9Jkuafa6OJbvY7STO7SQ2WtY5c0kjORrWnUiyM-aNc_4bzBE4/pub?gid=0&single=true&output=csv"
 
-curl $SRC -o ./src/data/scholarships.csv
+curl -L $SRC -o ./src/data/scholarships.csv
